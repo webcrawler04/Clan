a2571811272a60c3358b4a72be841ce0df7a12a8
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2019-03-10 13:45:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,55 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
+
     console.log('I am ready!');
+
 });
 
 client.on('message', message => {
+
     if (message.content === 'Holo sight on ak') {
+
     	message.reply('EWW');
+
   	}
-});
 
-client.on('ready', () => {
-    console.log('I am ready!');
 });
-
-client.on('message', message => {
-    if (message.content === 'ur mom gay') {
-    	message.reply('no u');
-  	}
-});
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ur gay') {
-    	message.reply('no sporky is');
-  	}
-});
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'lr300 is better than ak') {
-    	message.reply('*** insert ban here ***');
-  	}
-});
-
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'Dont do it') {
-    	message.reply('Why tho');
-  	}
-});
-
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
