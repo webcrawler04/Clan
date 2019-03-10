751276ff671fdce2ce4142c0a5327481c6dc99bd
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2019-03-10 11:22:52 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,14 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'Holo sight on ak') {
     	message.reply('EWW');
-        
-        client.on('message', message => {
+  	}
+});
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
     if (message.content === 'ur mom gay') {
     	message.reply('no u');
   	}
