fe59ca6388aa676504cc36ee11c091676f7478a5
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2019-03-10 11:09:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,18 +6,6 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-        
-        client.on('message', message => {
-    if (message.content === 'Hello') {
-    	message.reply('Hello, nice to meet you!');
-        
-        client.on('message', message => {
-    if (message.content === 'ur mom gey') {
-    	message.reply('no u');
-        
-        client.on('message', message => {
     if (message.content === 'Holo sight on ak.') {
     	message.reply('EWW');
   	}
