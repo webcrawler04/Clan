25380f2059c351531d400d37ff43a49af44619aa
webcrawler04 <48395890+webcrawler04@users.noreply.github.com>
2019-03-21 14:25:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,15 +11,5 @@ client.on('message', message => {
   	}
 });
 
-});
-
-client.on('message', message => {
-    if (message.content === 'ur mom gay') {
-    	message.reply('no u');
-  	}
-});
-
-
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
